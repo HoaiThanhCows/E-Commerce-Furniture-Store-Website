19da0860b481429c5a6793171d2f6d62555e5c3e
HoaiThanhCows <thothienti2@gmail.com>
2024-06-19 18:47:48 +0700

    doan

---
+++ BayaShop/WebBanHangOnline/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using PagedList;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Products
+        public ActionResult Index()
+        {
+            var items = db.Products.ToList();
+            
+            return View(items);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Detail(string alias,int id)
+        {
+            var item = db.Products.Find(id);
+            if (item != null)
+            {
+                db.Products.Attach(item);
+                item.ViewCount = item.ViewCount + 1;
+                db.Entry(item).Property(x => x.ViewCount).IsModified = true;
+                db.SaveChanges();
+            }
+            var countReview = db.Reviews.Where(x => x.ProductId == id).Count();
+            ViewBag.CountReview = countReview;
+            return View(item);
+        }
+        public ActionResult ProductCategory(string alias,int id)
+        {
+            var items = db.Products.ToList();
+            if (id > 0)
+            {
+                items = items.Where(x => x.ProductCategoryId == id).ToList();
+            }
+            var cate = db.ProductCategories.Find(id);
+            if (cate != null)
+            {
+                ViewBag.CateName = cate.Title;
+            }
+
+            ViewBag.CateId = id;
+            return View(items);
+        }
+
+        public ActionResult Partial_ItemsByCateId()
+        {
+            var items = db.Products.Where(x => x.IsHome && x.IsActive).Take(30).ToList();
+            return PartialView(items);
+        }
+
+        public ActionResult Partial_ProductSales()
+        {
+            var items = db.Products.Where(x => x.IsSale && x.IsActive).Take(30).ToList();
+            return PartialView(items);
+        }
+
+        public ActionResult pagelist(int? page)
+        {
+            var items = db.Products.ToList(); // Lấy dữ liệu từ cơ sở dữ liệu
+            int pageSize = 10; // Số lượng sản phẩm trên mỗi trang
+            int pageNumber = (page ?? 1);
+            var pagedList = items.ToPagedList(pageNumber, pageSize);
+            return View(pagedList);
+        }
+
+    }
+}
